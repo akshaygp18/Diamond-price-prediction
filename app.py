c659af020571b26e916626a7bdabc2323a01c06b
akshaygp18 <akshaygp41@gmail.com>
2023-04-21 18:03:45 +0530

    commited

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def predict_datapoint():
         return render_template('results.html',final_result=results)
 
 if __name__=="__main__":
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='127.0.0.1',debug=True)
 
 
 
